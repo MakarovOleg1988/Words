c8196695a5a78eb9b22b150d42ccd2cabf369ecc
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-11 13:37:15 +0530

    Добавьте файлы проекта.

---
+++ Words/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] _words = { "home", "programming", "C#", "Football", "work", "travel" };
+            string _letter = Console.ReadLine();
+            int _count = 0;
+
+            foreach (var item in _words)
+            {
+                if (item.Contains(_letter)) Console.WriteLine(item);
+                else _count += 1;
+            }
+
+            if (_count > (_words.Length - 1)) Console.Write("No match");
+        }
+    }
+}
